de86eaa4ec5dc6fce2e74cbd4f7ee99fe0342967
Tomasz Kurylik <fulcrumone@icloud.com>
2023-10-08 09:39:51 +0200

    Initial Commit

---
+++ Sources/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift of 
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//    - GitHub: https://github.com/FulcrumOne
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+
+import Foundation
---
+++ Tests/Locale++.swift
@@ -0,0 +1,17 @@
+//
+//  Locale++.swift of CalendarView
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//    - GitHub: https://github.com/FulcrumOne
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+
+import Foundation
+
+extension Locale {
+    static var polish: Locale { .init(identifier: "PL") }
+    static var english: Locale { .init(identifier: "EN") }
+}
