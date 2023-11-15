801aef2e151a344c5c34502c56c6bd8bd6e71905
Alina Petrovska <alina.petrovskaia92@gmail.com>
2023-11-15 15:02:07 +0100

    CAL-37: Implement Month Label
    feat:
    - Added MonthLabel protocol
    - Added DefaultMonthLabel
    
    refactor:
    - Updated code structure
    
    Ticket: https://mijick.atlassian.net/browse/CAL-37

---
+++ Sources/Public+MonthLabel.swift
@@ -0,0 +1,35 @@
+//
+//  Public+MonthLabel.swift of MijickCalendarView
+//
+//  Created by Alina Petrovska on 14.11.2023.
+//    - Mail: alina.petrovskaya@mijick.com
+//    - GitHub: https://github.com/Mijick
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+import SwiftUI
+
+public protocol MonthLabel: MView {
+    var month: Date { get }
+    var calendar: MCalendar { get }
+}
+
+// MARK: - Customizing View
+public extension MonthLabel {
+    func createContent() -> AnyView { createDefaultContent().erased() }
+}
+private extension MonthLabel {
+    func createDefaultContent() -> some View {
+        Text(monthText)
+            .font(.system(size: 16, weight: .semibold))
+            .foregroundColor(.black)
+    }
+}
+private extension MonthLabel {
+    var monthText: String { getString("MMMM y") }
+}
+
+// MARK: - Helpers
+public extension MonthLabel {
+    func getString(_ format: String) -> String { calendar.formatter().getString(from: month, format: format) }
+}
