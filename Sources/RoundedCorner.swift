12bc6de7c0f2d8ef29e0d084abecc0ff5556de52
Tomasz Kurylik <fulcrumone@icloud.com>
2023-11-15 14:22:11 +0100

    1

---
+++ Sources/RoundedCorner.swift
@@ -0,0 +1,23 @@
+//
+//  RoundedCorner.swift of CalendarView
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//    - GitHub: https://github.com/FulcrumOne
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+
+import SwiftUI
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
