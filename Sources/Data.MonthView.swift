666d0b134d7481242226e9f43196c74f125586d9
Tomasz Kurylik <fulcrumone@icloud.com>
2023-11-16 15:14:01 +0100

    1

---
+++ Sources/Data.MonthView.swift
@@ -0,0 +1,77 @@
+//
+//  Data.MonthView.swift of CalendarView
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//    - GitHub: https://github.com/FulcrumOne
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+
+import Foundation
+
+extension Data { struct MonthView {
+    let month: Date
+    let items: [[Date]]
+}}
+
+// MARK: - Generating Array
+extension [Data.MonthView] {
+    static func generate(_ calendar: MCalendar) -> Self {
+        let startDate = createStartDate(calendar)
+
+        return createItemsSet()
+            .map { createMonthDate($0, startDate, calendar) }
+            .map { createMonthViewData($0, calendar) }
+    }
+}
+private extension [Data.MonthView] {
+    static func createStartDate(_ calendar: MCalendar) -> Date { calendar.mDate(.now).startOfMonth() }
+    static func createItemsSet() -> Range<Int> { 0 ..< 12 * 25 }
+    static func createMonthDate(_ index: Int, _ startDate: Date, _ calendar: MCalendar) -> Date { calendar.mDate(startDate).adding(index, .month) }
+    static func createMonthViewData(_ monthStart: Date, _ calendar: MCalendar) -> Data.MonthView { .generate(monthStart, calendar) }
+}
+
+// MARK: - Generating Single Month
+private extension Data.MonthView {
+    static func generate(_ month: Date, _ calendar: MCalendar) -> Self {
+        let rawDates = createRawDates(month, calendar)
+        let groupedDates = groupDates(rawDates)
+
+        return .init(month: month, items: groupedDates)
+    }
+}
+private extension Data.MonthView {
+    static func createRawDates(_ month: Date, _ calendar: MCalendar) -> [Date] {
+        let monthStartDate = calendar.mDate(month).startOfMonth()
+        let monthStartWeekday = calendar.mDate(monthStartDate).getWeekday()
+
+        let items = (0..<numberOfRows * weekdaysNumber).map { createRawDate($0, monthStartDate, monthStartWeekday, calendar) }
+        return items
+    }
+    static func groupDates(_ rawDates: [Date]) -> [[Date]] {
+        rawDates
+            .enumerated()
+            .reduce(into: [], reduceRawDates)
+    }
+}
+private extension Data.MonthView {
+    static func createRawDate(_ index: Int, _ monthStartDate: Date, _ monthStartWeekday: MWeekday, _ calendar: MCalendar) -> Date {
+        let shiftIndex = {
+            let index = monthStartWeekday.rawValue - calendar.firstWeekday.rawValue
+            return index < 0 ? index + weekdaysNumber : index
+        }()
+        return calendar.mDate(monthStartDate).adding(index - shiftIndex, .day)
+    }
+    static func reduceRawDates(_ array: inout [[Date]], item: EnumeratedSequence<[Date]>.Iterator.Element) {
+        switch item.offset % weekdaysNumber == 0 {
+            case true: array.append([item.element])
+            case false: array[array.count - 1].append(item.element)
+        }
+    }
+}
+private extension Data.MonthView {
+    static var numberOfRows: Int { 5 }
+    static var weekdaysNumber: Int { MWeekday.weekdaysCount }
+}
