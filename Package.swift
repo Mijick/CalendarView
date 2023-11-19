bc8b02ced0040012d6dc68ec08fd0e36cc6f2dd0
alina-petrovska <alina.petrovskaia92@gmail.com>
2023-11-19 20:15:15 +0100

    CAL-52: Deploy tests on GitHub
    feat:
    - Added script to build and run test with github actions
    
    fix:
    - Updated package settings to make it possible to build project with github tools
    
    Ticket: https://mijick.atlassian.net/browse/CAL-52

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "MijickCalendarView",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14),
+        .macOS(.v11)
     ],
     products: [
         .library(name: "MijickCalendarView", targets: ["MijickCalendarView"])
