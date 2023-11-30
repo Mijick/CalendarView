56cf042295d86531debae268e888cce60e7ddd44
Tomasz Kurylik <fulcrumone@icloud.com>
2023-11-30 21:19:53 +0100

    1

--- Sources/Public/Configurables/Public+CalendarConfig.swift
+++ Sources/Public/Configurables/Public+CalendarConfig.swift
@@ -19,7 +19,17 @@ public extension CalendarConfig {
     func locale(_ value: Locale) -> Self { MCalendar.locale = value; return self }
 }
 
-// MARK: - Views
+// MARK: - Distances Between Objects
+public extension CalendarConfig {
+    func monthsTopPadding(_ value: CGFloat) -> Self { changing(path: \.monthsPadding.top, to: value) }
+    func monthsBottomPadding(_ value: CGFloat) -> Self { changing(path: \.monthsPadding.bottom, to: value) }
+    func monthLabelToDaysDistance(_ value: CGFloat) -> Self { changing(path: \.monthLabelDaysSpacing, to: value) }
+    func monthsSpacing(_ value: CGFloat) -> Self { changing(path: \.monthsSpacing, to: value) }
+    func daysVerticalSpacing(_ value: CGFloat) -> Self { changing(path: \.daysSpacing.vertical, to: value) }
+    func daysHorizontalSpacing(_ value: CGFloat) -> Self { changing(path: \.daysSpacing.horizontal, to: value) }
+}
+
+// MARK: - Custom Views
 public extension CalendarConfig {
     func monthLabel(_ builder: @escaping (Date) -> some MonthLabel) -> Self { changing(path: \.monthLabel, to: builder) }
     func weekdaysView(_ builder: @escaping () -> some WeekdaysView) -> Self { changing(path: \.weekdaysView, to: builder) }
@@ -35,6 +45,11 @@ public extension CalendarConfig {
 
 // MARK: - Internal
 public struct CalendarConfig: Configurable { public init() {}
+    private(set) var monthLabelDaysSpacing: CGFloat = 12
+    private(set) var monthsPadding: (top: CGFloat, bottom: CGFloat) = (12, 24)
+    private(set) var monthsSpacing: CGFloat = 24
+    private(set) var daysSpacing: (vertical: CGFloat, horizontal: CGFloat) = (2, 0)
+
     private(set) var monthLabel: (Date) -> any MonthLabel = DefaultMonthLabel.init
     private(set) var weekdaysView: () -> any WeekdaysView = DefaultWeekdaysView.init
     private(set) var dayView: (Date, Bool, Binding<Date?>?, Binding<MDateRange?>?) -> any DayView = DefaultDayView.init
