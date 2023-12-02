041260e9201a0695ebd1acbdc54082437469d852
Tomasz Kurylik <fulcrumone@icloud.com>
2023-12-03 00:18:28 +0100

    1

--- Sources/Public/Extensions/Public+MCalendarView.swift
+++ Sources/Public/Extensions/Public+MCalendarView.swift
@@ -12,7 +12,5 @@
 import SwiftUI
 
 extension MCalendarView {
-    public init(selectedDate: Binding<Date?>?, selectedRange: Binding<MDateRange?>?, configBuilder: (CalendarConfig) -> CalendarConfig = { $0 }) {
-        self.init(selectedDate, selectedRange, configBuilder)
-    }
+    public init(selectedDate: Binding<Date?>?, selectedRange: Binding<MDateRange?>?, configBuilder: (CalendarConfig) -> CalendarConfig = { $0 }) { self.init(selectedDate, selectedRange, configBuilder) }
 }
