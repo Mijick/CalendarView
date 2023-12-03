e2574acf48e6ef9c9e0acd41018f7f0d20ddb540
Tomasz Kurylik <fulcrumone@icloud.com>
2023-12-03 01:34:30 +0100

    1

--- Sources/Public/View Protocols/Public+WeekdaysView.swift
+++ Sources/Public/View Protocols/Public+WeekdaysView.swift
@@ -17,22 +17,16 @@ public protocol WeekdaysView: View {
 
 // MARK: - Customising View
 public extension WeekdaysView {
-    func createContent() -> AnyView { createDefaultContent().erased() }
+    func createContent() -> AnyView { createWeekdaysView().erased() }
     func createWeekdaysView() -> AnyView { createDefaultWeekdaysView().erased() }
     func createWeekdayLabel(_ weekday: MWeekday) -> AnyWeekdayLabel { createDefaultWeekDayLabel(weekday).erased() }
 
     var body: some View { createContent() }
 }
 private extension WeekdaysView {
-    func createDefaultContent() -> some View { createWeekdaysView() }
-    func createDefaultWeekdaysView() -> some View { HStack(spacing: 0) { ForEach(weekdays, id: \.self, content: createWeekdayItem) }}
+    func createDefaultWeekdaysView() -> some View { HStack(spacing: 0) { ForEach(MWeekday.allCases, id: \.self, content: createWeekdayItem) }}
     func createDefaultWeekDayLabel(_ weekday: MWeekday) -> DefaultWeekdayLabel { DefaultWeekdayLabel(weekday: weekday) }
 }
 private extension WeekdaysView {
     func createWeekdayItem(_ weekday: MWeekday) -> some View { createWeekdayLabel(weekday).frame(maxWidth: .infinity) }
-}
-
-// MARK: Helpers
-public extension WeekdaysView {
-    var weekdays: [MWeekday] { MWeekday.allCases }
 }
