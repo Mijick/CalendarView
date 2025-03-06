49d867b4cdda6d6de511dfc034dcdd6412773856
Riski Wahyu <riski.wr@hotmail.com>
2025-03-06 22:58:13 +0700

    fix: selectedDate not updating

--- Sources/Internal/Views/MCalendarView.swift
+++ Sources/Internal/Views/MCalendarView.swift
@@ -8,20 +8,24 @@
 //
 //  Copyright ©2023 Mijick. Licensed under MIT License.
 
-
 import SwiftUI
 
 public struct MCalendarView: View {
-    @StateObject var selectedData: Data.MCalendarView
+    @Binding var selectedDate: Date?
+    @Binding var selectedRange: MDateRange?
     let monthsData: [Data.MonthView]
     let configData: CalendarConfig
 
-
-    init(_ selectedDate: Binding<Date?>?, _ selectedRange: Binding<MDateRange?>?, _ configBuilder: (CalendarConfig) -> CalendarConfig) {
-        self._selectedData = .init(wrappedValue: .init(selectedDate, selectedRange))
+    init(
+        _ selectedDate: Binding<Date?>, _ selectedRange: Binding<MDateRange?>,
+        _ configBuilder: (CalendarConfig) -> CalendarConfig
+    ) {
+        _selectedDate = selectedDate
+        _selectedRange = selectedRange
         self.configData = configBuilder(.init())
         self.monthsData = .generate()
     }
+
     public var body: some View {
         VStack(spacing: 12) {
             createWeekdaysView()
@@ -29,49 +33,58 @@ public struct MCalendarView: View {
         }
     }
 }
-private extension MCalendarView {
-    func createWeekdaysView() -> some View {
+extension MCalendarView {
+    fileprivate func createWeekdaysView() -> some View {
         configData.weekdaysView().erased()
     }
-    func createScrollView() -> some View { ScrollViewReader { reader in
-        ScrollView(showsIndicators: false) {
-            LazyVStack(spacing: configData.monthsSpacing) {
-                ForEach(monthsData, id: \.month, content: createMonthItem)
+    fileprivate func createScrollView() -> some View {
+        ScrollViewReader { reader in
+            ScrollView(showsIndicators: false) {
+                LazyVStack(spacing: configData.monthsSpacing) {
+                    ForEach(monthsData, id: \.month, content: createMonthItem)
+                }
+                .padding(.top, configData.monthsPadding.top)
+                .padding(.bottom, configData.monthsPadding.bottom)
+                .background(configData.monthsViewBackground)
             }
-            .padding(.top, configData.monthsPadding.top)
-            .padding(.bottom, configData.monthsPadding.bottom)
-            .background(configData.monthsViewBackground)
+            .onAppear { scrollToDate(reader, animatable: false) }
+            .onChange(of: selectedDate) { newdate in
+                guard let date = newdate else { return }
+                withAnimation {
+                    reader.scrollTo(date.start(of: .month), anchor: .top)
+                }
+            }
         }
-        .onAppear() { scrollToDate(reader, animatable: false) }
-        .onChange(of: configData.scrollDate) { _ in scrollToDate(reader, animatable: true) }
-    }}
+    }
 }
-private extension MCalendarView {
-    func createMonthItem(_ data: Data.MonthView) -> some View {
+extension MCalendarView {
+    fileprivate func createMonthItem(_ data: Data.MonthView) -> some View {
         VStack(spacing: configData.monthLabelDaysSpacing) {
             createMonthLabel(data.month)
             createMonthView(data)
         }
     }
 }
-private extension MCalendarView {
-    func createMonthLabel(_ month: Date) -> some View {
+extension MCalendarView {
+    fileprivate func createMonthLabel(_ month: Date) -> some View {
         configData.monthLabel(month)
             .erased()
             .onAppear { onMonthChange(month) }
     }
-    func createMonthView(_ data: Data.MonthView) -> some View {
-        MonthView(selectedDate: $selectedData.date, selectedRange: $selectedData.range, data: data, config: configData)
+    fileprivate func createMonthView(_ data: Data.MonthView) -> some View {
+        MonthView(
+            selectedDate: $selectedDate, selectedRange: $selectedRange, data: data,
+            config: configData)
     }
 }
 
 // MARK: - Modifiers
-private extension MCalendarView {
-    func scrollToDate(_ reader: ScrollViewProxy, animatable: Bool) {
+extension MCalendarView {
+    fileprivate func scrollToDate(_ reader: ScrollViewProxy, animatable: Bool) {
         guard let date = configData.scrollDate else { return }
 
         let scrollDate = date.start(of: .month)
         withAnimation(animatable ? .default : nil) { reader.scrollTo(scrollDate, anchor: .center) }
     }
-    func onMonthChange(_ date: Date) { configData.onMonthChange(date) }
+    fileprivate func onMonthChange(_ date: Date) { configData.onMonthChange(date) }
 }
